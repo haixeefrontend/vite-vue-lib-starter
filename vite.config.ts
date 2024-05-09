750b39b5e1bbc63253a5ecd6feb87cc31fbfc600
Maiko Tan <maiko.tan.coding@gmail.com>
2024-05-09 14:39:01 +0800

    chore: update dependencies and make publint happy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+import { copyFileSync } from 'node:fs'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
 
 import packageJson from './package.json'
 
@@ -11,7 +14,17 @@ function getPackageName() {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      afterBuild() {
+        // Publint requires a .d.cts type definition file
+        // https://publint.dev/rules#export_types_invalid_format
+        copyFileSync('dist/index.d.ts', 'dist/index.d.cts')
+      },
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
